eae2c1dd47c458592b000935a01d0213bdfd9e8f
Anastasiya <nastya.9323@mail.ru>
2025-07-01 22:23:36 +0300

    1

---
+++ src/AddAToB.php
@@ -0,0 +1,14 @@
+<?php 
+class AddAToB{
+    protected $a;
+    protected $b;
+
+    public function _construct($a, $b){
+        $this->a = $a;
+        $this->b = $b;
+    }
+
+    public function add($a, $b){
+return $a+$b;
+    }
+}
